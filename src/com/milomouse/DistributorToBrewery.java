fe0ad180052ab2790e7dfea864b64e820c6961db
LauraBOwensKY <LauraBOwens@gmail.com>
2019-07-23 16:14:59 -0400

    Testing DistributorToBrewery Method to verify list contents

--- src/com/milomouse/DistributorToBrewery.java
+++ src/com/milomouse/DistributorToBrewery.java
@@ -1,13 +1,20 @@
 package com.milomouse;
 
+import static jdk.nashorn.internal.objects.NativeArray.lastIndexOf;
+import static jdk.nashorn.internal.runtime.ScriptingFunctions.readLine;
+
+import java.io.IOException;
 import java.util.List;
 
 public class DistributorToBrewery {
 
 
-    public void displayDistributorToBrewery(List<String> distributorRecords, List<String> breweryRecords) {
-        System.out.printf("%s     %s     %n", distributorRecords.indexOf("Louisville"), breweryRecords.indexOf("Louisville"));
+    public void displayDistributorToBrewery(List<String> distributorRecords, List<String> breweryRecords)
+        throws IOException {
 
+        System.out.println(distributorRecords);
+        String readLine = readLine("Press any key to continue:  ");
+        System.out.println(breweryRecords);
     } // displayDistributorToBrewery
 
 
